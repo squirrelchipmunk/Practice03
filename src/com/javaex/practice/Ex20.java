978bc25f511adeea6eba3e8bde51cc830d746566
이원준 <ggoong8@gmail.com>
2021-11-17 20:51:52 +0900

    Practice03 반복문

--- src/com/javaex/practice/Ex20.java
+++ src/com/javaex/practice/Ex20.java
@@ -9,8 +9,9 @@ public class Ex20 {
 		int randNum;
 		int inputNum;
 		String answer;
+		boolean yesOrNo = true;
 		
-		while(true) {
+		while(yesOrNo) {
 			System.out.println("=========================");
 			System.out.println("    [숫자맞추기게임 시작]");
 			System.out.println("=========================");
@@ -32,10 +33,20 @@ public class Ex20 {
 				}
 			}
 			System.out.println("게임을 종료하시겠습니까? (y/n)");
-			answer=sc.next();
-			if("y".equals(answer)) {
-				break;
+			while(true) {
+				answer=sc.next();
+				if("y".equals(answer)) {
+					yesOrNo=false;
+					break;
+				}
+				else if("n".equals(answer)) {
+					break;
+				}
+				else {
+					System.out.println("y또는 n으로 입력해주세요");
+				}
 			}
+			
 		}
 		
 		System.out.println("=========================");
