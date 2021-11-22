477d2edcfa452c2e9a4441a27f282c26fbba986e
이원준 <ggoong8@gmail.com>
2021-11-22 13:49:52 +0900

    Practice03

--- src/com/javaex/practice/Ex09.java
+++ src/com/javaex/practice/Ex09.java
@@ -4,8 +4,8 @@ public class Ex09 {
 
 	public static void main(String[] args) {
 
-		for(int i=0;i<10;i++) {
-			for(int j=1;j<=10;j++) {
+		for(int i=1;i<=10;i++) {
+			for(int j=0;j<10;j++) {
 				System.out.print((i+j)+"\t");
 			}
 		
